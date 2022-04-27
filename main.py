848e78e5b37f95766842b8e9d4aa8314fe8eccac
Mario Torres <mariotorreslagos@gmail.com>
2022-04-27 21:42:16 +0000

    adding multiple users to db with sqlalchemy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///users.db', echo=True)
+engine = create_engine('sqlite:///users.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
 
 class User(Base):
@@ -20,9 +23,26 @@ if __name__ == '__main__':
 
   mario_user = User(name='Mario', fullname='Mario Alfredo Torres Lagos', nickname='Negro')
 
-  print(mario_user.name)
-  print(mario_user.fullname)
-  print(mario_user.nickname)
+  # print(mario_user.name)
+  # print(mario_user.fullname)
+  # print(mario_user.nickname)
+  # session.add(mario_user)
+  # #print(session.new)
+  # session.commit()
+  # print(mario_user.id)
+
+  new_users = [
+    User(name='Francisco', fullname='Francisco Javier Torres Lagos', nickname='Mac'),
+    User(name='Maria', fullname='Maria Paz Torres Lagos', nickname='Mary'),
+    User(name='Felipe', fullname='Felipe Ignacio Torres Lagos', nickname='Pollin'),
+  ]
+
+  session.add_all(new_users)
+  session.commit()
+
+  for user in new_users:
+    print(user.id)    
+  
 
   
 
